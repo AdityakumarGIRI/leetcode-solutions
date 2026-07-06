febc34ad37b81cbdaeafd8f5393428a60dfc05e7
AdityakumarGIRI <aditya24037@iiitd.ac.in>
2026-07-07 02:13:13 +0530

    Sync LeetCode solution: Remove Covered Intervals (C++)

---
+++ Medium/1222_Remove_Covered_Intervals.cpp
@@ -0,0 +1,35 @@
+/*
+ * Problem: Remove Covered Intervals
+ * Problem ID: 1222
+ * Difficulty: Medium
+ * Language: C++
+ * Runtime: 0 ms
+ * Memory: 15.2 MB
+ * Synced From: LeetCode
+ * Date: 2026-07-06
+ */
+
+class Solution {
+public:
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end(),[](const vector<int>& a, const vector<int>& b){
+            if(a[0]==b[0]){
+                return a[1]>b[1];
+
+            }
+            return a[0]<b[0];
+            
+        });
+        int ans=1;
+        int maxend = intervals[0][1];
+        for(int i =0 ; i < intervals.size();i++){
+            if(intervals[i][1]<=maxend){
+                continue;
+
+            }
+            ans++;
+            maxend = intervals[i][1];
+        }
+        return ans;
+    }
+};
