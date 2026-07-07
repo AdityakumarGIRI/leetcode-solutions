e2260e33ec8d6392c5469a5edb0ff924e705ba65
AdityakumarGIRI <aditya24037@iiitd.ac.in>
2026-07-08 00:46:40 +0530

    Sync LeetCode solution: Divisor Game (C++)

---
+++ Easy/1086_Divisor_Game.cpp
@@ -0,0 +1,19 @@
+/*
+ * Problem: Divisor Game
+ * Problem ID: 1086
+ * Difficulty: Easy
+ * Language: C++
+ * Runtime: 0 ms
+ * Memory: 7.6 MB
+ * Synced From: LeetCode
+ * Date: 2026-07-07
+ */
+
+class Solution {
+public:
+    bool divisorGame(int n) {
+
+        return n%2 == 0;
+        
+    }
+};
