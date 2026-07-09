0d7cf79ec54f27e1113fb5dbdf5a6d3f40d9579d
AdityakumarGIRI <aditya24037@iiitd.ac.in>
2026-07-10 00:57:51 +0530

    Sync LeetCode solution: Path Existence Queries in a Graph I (C++)

---
+++ Medium/3838_Path_Existence_Queries_in_a_Graph_I.cpp
@@ -0,0 +1,34 @@
+/*
+ * Problem: Path Existence Queries in a Graph I
+ * Problem ID: 3838
+ * Difficulty: Medium
+ * Language: C++
+ * Runtime: 12 ms
+ * Memory: 223.9 MB
+ * Synced From: LeetCode
+ * Date: 2026-07-09
+ */
+
+class Solution {
+public:
+    vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
+        vector<int> comp(n);
+        int id = 0;
+        comp[0] = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] - nums[i - 1] > maxDiff)
+                id++;
+            comp[i] = id;
+        }
+
+        vector<bool> ans;
+        ans.reserve(queries.size());
+
+        for (auto &q : queries) {
+            ans.push_back(comp[q[0]] == comp[q[1]]);
+        }
+
+        return ans;
+    }
+};
