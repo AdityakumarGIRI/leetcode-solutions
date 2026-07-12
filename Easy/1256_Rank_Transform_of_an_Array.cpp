6b21eeb10ee8f616927461874442df879b3eaaa0
AdityakumarGIRI <aditya24037@iiitd.ac.in>
2026-07-13 01:33:59 +0530

    Sync LeetCode solution: Rank Transform of an Array (C++)

---
+++ Easy/1256_Rank_Transform_of_an_Array.cpp
@@ -0,0 +1,34 @@
+/*
+ * Problem: Rank Transform of an Array
+ * Problem ID: 1256
+ * Difficulty: Easy
+ * Language: C++
+ * Runtime: 79 ms
+ * Memory: 45.8 MB
+ * Synced From: LeetCode
+ * Date: 2026-07-12
+ */
+
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        vector<int>temp = arr;
+        sort(temp.begin(),temp.end());
+
+        map<int,int> rank;
+        int currentrank =1;
+        for(int i = 0 ; i < temp.size() ; i++){
+            if(rank.find(temp[i])== rank.end()){
+                rank[temp[i]] = currentrank;
+                currentrank++;
+            }
+        }
+        vector<int>answer;
+
+        for(int i = 0 ;i < arr.size();i++){
+            answer.push_back(rank[arr[i]]);
+
+        }
+        return answer;
+    }
+};
